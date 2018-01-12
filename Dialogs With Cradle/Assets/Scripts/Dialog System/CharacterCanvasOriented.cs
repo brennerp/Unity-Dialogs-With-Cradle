89b54b4a5b7b84b52d197eca60ac0ea99e72cfb6
Brenner Pacelli <brenner_pacelli@hotmail.com>
2018-01-12 16:07:46 -0200

    Update CharacterCanvasOriented.cs

--- Dialogs With Cradle/Assets/Scripts/Dialog System/CharacterCanvasOriented.cs
+++ Dialogs With Cradle/Assets/Scripts/Dialog System/CharacterCanvasOriented.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-///Old Lady Game Project
-///Written by Brenner Pacelli on November 13th, 2017
-///Last updated on November 13th, 2017
-///CharacterCanvasOriented is an interface for objects in the canvas that need to align with objects in scene
-
 public interface CharacterCanvasOriented {
 
 	RectTransform Rect { get; }
