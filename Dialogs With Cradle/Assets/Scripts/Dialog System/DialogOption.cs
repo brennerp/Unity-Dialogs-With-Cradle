aa951be34d355095f5302a1fe935f26d75ba9bf4
Brenner Pacelli <brenner_pacelli@hotmail.com>
2018-01-12 16:10:48 -0200

    Update DialogOption.cs

--- Dialogs With Cradle/Assets/Scripts/Dialog System/DialogOption.cs
+++ Dialogs With Cradle/Assets/Scripts/Dialog System/DialogOption.cs
@@ -5,9 +5,6 @@ using UnityEngine.UI;
 using Cradle;
 using Cradle.StoryFormats.Sugar;
 
-///Old Lady Game Project
-///Written by Brenner Pacelli on October 25th, 2017
-///Last updated on December 14th, 2017
 ///DialogOption manages the display of one dialog link
 
 namespace DialogSystem {
